086321327753f01ef6ddec9542bac3fab3f973e5
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-08-19 15:04:22 +0000

    finish[1.4]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  def hello
+    render html: "hola, mundo!"
+  end 
+  
+  def goodbye
+    render html: "goodbye, world!"
+  end 
+  
 end
